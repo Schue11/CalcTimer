7386ee993473174de75b30f953147eeb06f0df9e
David S <z1887008@students.niu.edu>
2023-11-21 12:40:37 -0600

    Updated Calculations

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿//Fixed Code
 using System;
+using System.Data.SqlTypes;
 using System.Diagnostics;
+using System.Globalization;
 
 class Program
 {
@@ -8,9 +10,17 @@ class Program
     {
         int n = 800000;
         double [,] numbers;
+        float[,] numb;
         Stopwatch timer  =new Stopwatch();
 
         numbers = GenRandomNumbers(n);
+        numb = new float[n,3];
+
+        for(int i = 0; i<n; ++i){
+
+            numb[i,0] = (float)numbers[i,0];
+            numb[i,1] = (float)numbers[i,1];
+        }
 
         timer.Restart();
         AddNumbers(numbers, n);
@@ -18,15 +28,34 @@ class Program
 
         Console.WriteLine("Additions doubles");
         Console.WriteLine("Elapsed Time = " + timer.ElapsedMilliseconds + " ms" + timer.ElapsedTicks + "ticks");
+        float addTicks = (float)timer.ElapsedTicks;
+
 
         timer.Restart();
-        AddNumbers(numbers, n);
+        AddNumbers(numb, n);
         timer.Stop();
 
         Console.WriteLine("Additions floats");
         Console.WriteLine("Elapsed Time = " + timer.ElapsedMilliseconds + " ms" + timer.ElapsedTicks + "ticks");
+        double adTicks = (double)timer.ElapsedTicks;
+        Console.WriteLine("Ratio = " + addTicks/adTicks);
 
+        timer.Restart();
+        SquareMultiply(numbers, n);
+        timer.Stop();
 
+        Console.WriteLine("Squaring by multiplication");
+        Console.WriteLine("Elapsed time = "+ timer.ElapsedMilliseconds + "ms" + timer.ElapsedTicks + "ticks");
+        int Squared = (int)timer.ElapsedTicks;
+        Console.WriteLine("Ratio = "+ addTicks/Squared);
+
+        timer.Restart();
+        Sq(numbers,n);
+        timer.Stop();
+        Console.WriteLine("Squaring function");
+        Console.WriteLine("Elapsed time = " + timer.ElapsedMilliseconds + "ms" + timer.ElapsedTicks + "ticks");
+        int SquareMath = (int)timer.ElapsedTicks;
+        Console.WriteLine("Ratio = " + addTicks/SquareMath);
 
 
         // int i;
@@ -64,4 +93,23 @@ class Program
         }
         
     }
+
+    static void SquareMultiply(double[,] nums,int count)
+    {
+        int i;
+        for(i=0; i<count; ++i){
+            nums[i,2] = nums[i,1] * nums [i,1];
+    }
+    }
+    
+    static void Sq(double[,] nums, int count)
+    {
+        int i;
+        for(i=0; i<count; ++i){
+            nums[i,2] = Math.Pow(nums[i,0],nums[i,1]);
+
+        }
+    }
 }
+
+
